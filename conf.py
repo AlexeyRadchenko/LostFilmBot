46ac2cf18dee4b7ad0c9e0634a699d307a14bb99
AlexeyRadchenko <novame@mail.ru>
2018-05-02 22:04:52 +0300

    Delete conf.py

--- conf.py
+++
@@ -1,5 +0,0 @@
-"""303355444:AAH4hG7R9ySLF5ySoQeb7Y6o_tu5rIaGqw8"""
-"""lfbot:nDeugYzbeM4h"""
-TOKEN = '303355444:AAH4hG7R9ySLF5ySoQeb7Y6o_tu5rIaGqw8'
-SOCKS_USER = 'lfbot'
-SOCKS_PASS = 'nDeugYzbeM4h'
